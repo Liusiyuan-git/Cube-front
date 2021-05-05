6bf6981b81154032825a328f3634698f0dacd278
liusiyuan <945212191@qq.com>
2021-05-05 17:57:06 +0800

    Cube-front 2021/5/5

---
+++ common/app.js
@@ -0,0 +1,9 @@
+const angular = require("angular")
+const angularAnimate = require("angular-animate")
+require("angular-file-upload-shim/dist/angular-file-upload")
+require("moment").locale("zh-cn")
+require("angular-moment")
+const app = angular.module("tool.common",[angularAnimate, "angularFileUpload", "angularMoment"])
+.constant("monet", require("moment"));
+require("./common.js");
+export default app.name;
