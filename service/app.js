d96e276df708c04777a0caea63c98d22a0cf0d4d
liusiyuan <945212191@qq.com>
2021-05-25 23:34:58 +0800

    Cube-front 2021/5/25

---
+++ service/app.js
@@ -0,0 +1,14 @@
+import "es6-promise/auto"
+
+import angular from "angular"
+import uiRouter from "angular-ui-router"
+import ocLazyLoad from "oclazyload"
+import ngFileUpload from "ng-file-upload"
+import toolCommon from "../common/app"
+
+window.app = angular.module("myApp", [uiRouter, ocLazyLoad, toolCommon, ngFileUpload]);
+let module_json_loaders = require.context("./", true, /module\.json$/)
+module_json_loaders.keys().forEach(function (_key){
+    window.app.config(["$stateProvider", module_json_loaders(_key)]);
+})
+// angular.bootstrap(document, ["myApp"]);
