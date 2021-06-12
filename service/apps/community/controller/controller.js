79fd7f42bfd59ab8ed3c2465692feb910269fba5
liusiyuan <945212191@qq.com>
2021-06-12 22:55:31 +0800

    Cube-front 2021/6/12

---
+++ service/apps/community/controller/controller.js
@@ -0,0 +1,21 @@
+let app = require("../../app")
+import "../style/style.scss"
+
+app.controller("communityCtrl", ["$rootScope", "$scope", "$state", "$timeout", 'dataService',
+    function ($rootScope, $scope, $state, $timeout, dataService) {
+    $scope.initCommunity = function () {
+        $rootScope.cubelocation = "community"
+        $scope.loginStatusCheck()
+    };
+
+    $scope.loginStatusCheck = function () {
+        if(!$rootScope.login){
+            $rootScope.loginStatusCheck().then(function (data){
+                if (data.success){
+                    $rootScope.userId = data["cubeId"];
+                    $rootScope.login = true;
+                }
+            })
+        }
+    }
+}])
