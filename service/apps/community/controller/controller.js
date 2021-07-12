8cf6083b3a8204061c056f397719651de13b3a79
liusiyuan <945212191@qq.com>
2021-07-12 21:32:50 +0800

    Cube-front 2021/7/12

--- service/apps/community/controller/controller.js
+++ service/apps/community/controller/controller.js
@@ -3,19 +3,24 @@ import "../style/style.scss"
 
 app.controller("communityCtrl", ["$rootScope", "$scope", "$state", "$timeout", 'dataService',
     function ($rootScope, $scope, $state, $timeout, dataService) {
-    $scope.initCommunity = function () {
-        $rootScope.cubelocation = "community"
-        $scope.loginStatusCheck()
-    };
+        $scope.initCommunity = function () {
+            $rootScope.cubelocation = "community"
+            $scope.loginStatusCheck()
+        };
 
-    $scope.loginStatusCheck = function () {
-        if(!$rootScope.login){
-            $rootScope.loginStatusCheck().then(function (data){
-                if (data.success){
-                    $rootScope.userId = data["cubeId"];
-                    $rootScope.login = true;
-                }
-            })
+        $scope.loginStatusCheck = function () {
+            if (!$rootScope.login) {
+                $rootScope.loginStatusCheck().then(function (data) {
+                    if (data.success) {
+                        $rootScope.userId = data["cubeId"];
+                        $rootScope.login = true;
+                        $scope.rabbitmq()
+                    }
+                })
+            }
+        };
+
+        $scope.rabbitmq = function () {
+
         }
-    }
-}])
+    }])
