d5270b586ef6472632400c2d450a749f95c9f9f5
liusiyuan <945212191@qq.com>
2021-07-15 20:48:40 +0800

    Cube-front 2021/7/15

--- service/apps/talking/controller/controller.js
+++ service/apps/talking/controller/controller.js
@@ -1,3 +1,5 @@
+import E from "wangeditor";
+
 let app = require("../../app")
 import "../style/style.scss"
 
@@ -7,12 +9,45 @@ app.controller("talkingCtrl", ["$rootScope", "$scope", "$state", "$timeout", 'da
             let frame = document.getElementById("container");
             frame.className = "container in";
         }, 300);
-        $scope.contentDataGet()
+        $scope.talkEditorInit()
+        // $scope.contentDataGet()
     };
 
     $scope.menuSelect = function (key) {
         $scope.talkingMenu.forEach(function (item) {
             item.select = item.key === key
+        })
+    };
+
+    $scope.talkEditorInit = function () {
+        $scope.talkEditor = new E('#talk-toolbar', '#talk-text');
+        $scope.talkEditor.config.height = 1200;
+        $scope.talkEditor.config.menus = [
+            'emoticon'
+        ]
+        $scope.talkEditor.create();
+    };
+
+    $scope.talkSend = function () {
+        let text = $scope.talkEditor.txt.text()
+        if (text === '') {
+            $rootScope.cubeWarning('warning', '内容不能为空')
+            return null
+        }
+        if (!$rootScope.userId) {
+            $rootScope.cubeWarning('error', '请先登录')
+            return null
+        }
+        dataService.callOpenApi('send.talk', {
+            cubeid: $rootScope.userId,
+            text: text
+        }, 'private').then(function (data) {
+            if (data.success) {
+                $rootScope.cubeWarning('success', '发布成功')
+            } else {
+                $rootScope.cubeWarning('error', data.msg || '发布出错')
+            }
+
         })
     };
 
@@ -24,7 +59,7 @@ app.controller("talkingCtrl", ["$rootScope", "$scope", "$state", "$timeout", 'da
         key: "care",
         name: "关注",
         select: false
-    },{
+    }, {
         key: "talk",
         name: "树洞",
         select: false
