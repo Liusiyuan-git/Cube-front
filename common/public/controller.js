fa9b722d339c410f1ffdda657d2ab38a608a3270
liusiyuan <945212191@qq.com>
2021-07-26 21:01:53 +0800

    Cube-front 2021/7/26

--- common/public/controller.js
+++ common/public/controller.js
@@ -1,5 +1,9 @@
 import app from "../base"
 import Swal from 'sweetalert2/src/sweetalert2.js'
+import "../component/page/jquery-1.7.2.min"
+import "../component/page/jqPaginator.min"
+import "../component/page/myPage.css"
+import "../component/page/bootstrap.min.css"
 // import 'sweetalert2/src/sweetalert2.scss'
 //import Swal from 'sweetalert2'
 import "../styles/common.color.scss"
@@ -88,6 +92,30 @@ app.controller("PublicController", ["$rootScope", "$scope", "$state", '$q', 'dat
             confirmButtonText: yes,
             denyButtonText: no,
         })
+    };
+
+    $rootScope.loadpage = function (f) {
+        let myPageCount = parseInt($("#PageCount").val());
+        let myPageSize = parseInt($("#PageSize").val());
+        let countindex = myPageCount % myPageSize > 0 ? (myPageCount / myPageSize) + 1 : (myPageCount / myPageSize);
+        $("#countindex").val(countindex);
+
+        $.jqPaginator('#pagination', {
+            totalPages: parseInt($("#countindex").val()),
+            visiblePages: parseInt($("#visiblePages").val()),
+            currentPage: 1,
+            first: '<li class="first"><a href="javascript:;">首页</a></li>',
+            prev: '<li class="prev"><a href="javascript:;"><i class="arrow arrow2"></i>上一页</a></li>',
+            next: '<li class="next"><a href="javascript:;">下一页<i class="arrow arrow3"></i></a></li>',
+            last: '<li class="last"><a href="javascript:;">末页</a></li>',
+            page: '<li class="page"><a href="javascript:;">{{page}}</a></li>',
+            // onPageChange: function (num, type) {
+            //     if (type == "change") {
+            //         exeData(num, type);
+            //     }
+            // }
+            onPageChange: f
+        });
     };
 
 }])
