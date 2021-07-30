c8657c2c5d928ae86ab224b73e0854e40f26c67f
liusiyuan <945212191@qq.com>
2021-07-30 22:07:38 +0800

    Cube-front 2021/7/30

--- service/apps/home/controller/controller.js
+++ service/apps/home/controller/controller.js
@@ -8,18 +8,77 @@ app.controller("homeCtrl", ["$rootScope", "$scope", "$state", "$timeout", 'dataS
             frame.className = "container in";
         }, 300);
         $scope.contentDataGet()
+        $scope.cubeInformationGet()
+        $scope.cubeViewGet()
+        $timeout(function () {
+            $scope.collectionGet()
+        }, 500)
+        $scope.cubetest = [1, 2, 3, 4, 5]
     };
+
+    $scope.echarts = function (count) {
+        let echarts = require("echarts/dist/echarts")
+        let month = (new Date()).getMonth() + 1;
+        let chartElement = document.getElementById('active-echarts')
+        chartElement.style.height = JSON.stringify(chartElement.clientWidth) + "px"
+        let myChart = echarts.init(chartElement);
+
+        let option = {
+            title: {
+                text: '每月访问量'
+            },
+            tooltip: {},
+            xAxis: {
+                data: [month - 5, month - 4, month - 3, month - 2, month - 1, month]
+            },
+            yAxis: {},
+            series: [{
+                name: '访问数',
+                type: 'line',
+                color: ['#03a9f4'],
+                data: [count[0], count[1], count[2], count[3], count[4], count[5]]
+            }]
+        };
+        myChart.setOption(option);
+    }
 
     $scope.menuSelect = function (key) {
         $scope.homeMenu.forEach(function (item) {
             item.select = item.key === key
         })
+        $scope.contentDataGet(key)
     };
 
+    $scope.cubeInformationGet = function () {
+        dataService.callOpenApi("cube.information.get", {}, "common").then(function (data) {
+            if (data.success) {
+                $scope.cubeInformation = data.content
+            }
+        })
+    };
 
-    $scope.contentDataGet = function () {
+    $scope.collectionGet = function () {
+        if (!$rootScope.userId) {
+            return null
+        }
+        dataService.callOpenApi("cube.collection.get", {"cubeid": $rootScope.userId}, "private").then(function (data) {
+            if (data.success) {
+                $scope.cubeCollection = data.content
+            }
+        })
+    };
+
+    $scope.cubeViewGet = function () {
+        dataService.callOpenApi("cube.view.get", {}, "common").then(function (data) {
+            if (data.success) {
+                $scope.echarts(data.content)
+            }
+        })
+    };
+
+    $scope.contentDataGet = function (mode = "new") {
         $rootScope.cubeLoading("加载中...")
-        dataService.callOpenApi("blog.get", {}, "common").then(function (data) {
+        dataService.callOpenApi("blog.get", {"mode": mode}, "common").then(function (data) {
             $rootScope.swal.close()
             if (data.success) {
                 if (data.content) {
@@ -33,8 +92,15 @@ app.controller("homeCtrl", ["$rootScope", "$scope", "$state", "$timeout", 'dataS
                     })
                 }
                 $scope.content = data.content
+                // $("#PageCount").val("10");
+                // $("#PageSize").val("5");
+                // $rootScope.loadpage()
             }
         })
+    };
+
+    $scope.collectionSelect = function (id) {
+        $state.go("blog", {id: id})
     };
 
     $scope.blog = function (id) {
