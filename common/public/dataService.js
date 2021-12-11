7d01757637699a686ffa9b23f80f1604c78b3511
liusiyuan <945212191@qq.com>
2021-12-11 16:11:30 +0800

    Cube-front 2021/12/11

--- common/public/dataService.js
+++ common/public/dataService.js
@@ -5,7 +5,9 @@ app.service("dataService", ["$rootScope", "$http", "$q",
         return {
             callOpenApi: function (api_name, api_params = {}, mode = "main") {
                 let defer = $q.defer()
-                let _url = "http://" + "127.0.0.1:8080" + "/api/" + mode + "/" + api_name;
+                // let _url = "http://" + "127.0.0.1:8080" + "/api/" + mode + "/" + api_name;
+                // let _url = "http://43.155.100.23:8080" + "/api/" + mode + "/" + api_name;
+                let _url = "https://www.cube.fan:8080" + "/api/" + mode + "/" + api_name;
                 $http.post(_url, api_params).then(function (data) {
                     success_handler(data.data)
                 }, function (data) {
@@ -19,7 +21,6 @@ app.service("dataService", ["$rootScope", "$http", "$q",
                 function fail_handler(data) {
                     defer.reject(data)
                 }
-
                 return defer.promise;
             },
         }
